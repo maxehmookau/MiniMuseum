e46678ba980f325307ff612eb141899c30b17647
Max Woolf <maximus@zingysaturn.co.uk>
2014-11-06 13:22:50 +0000

    simple example of finding only immediate beacons

--- MiniMuseum/ViewController.h
+++ MiniMuseum/ViewController.h
@@ -1,10 +1,12 @@
 #import <UIKit/UIKit.h>
 #import <CoreLocation/CoreLocation.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <CLLocationManagerDelegate>
 
 @property (nonatomic, strong) CLLocationManager *locationManager;
 @property (nonatomic, strong) CLBeaconRegion *beaconRegion;
+
+@property (nonatomic, strong) CLBeacon *closestBeacon;
 
 @end
 
--- MiniMuseum/ViewController.m
+++ MiniMuseum/ViewController.m
@@ -9,9 +9,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     _locationManager = [[CLLocationManager alloc] init];
-    NSUUID *uuid = [NSUUID UUID]; // Definitely need to change this when beacons arrive
+    NSUUID *uuid = [[NSUUID alloc] initWithUUIDString:@"B9407F30-F5F8-466E-AFF9-25556B57FE6D"]; // Definitely need to change this when beacons arrive
     _beaconRegion = [[CLBeaconRegion alloc] initWithProximityUUID:uuid
                                                        identifier:@"com.substrakt.minimuseum"];
+    [_locationManager setDelegate:self];
+    [_locationManager startMonitoringForRegion:_beaconRegion];
     
 }
 
@@ -19,5 +21,38 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - CLLocationManagerDelegate
+- (void)locationManager:(CLLocationManager *)manager didDetermineState:(CLRegionState)state forRegion:(CLRegion *)region {
+    switch (state) {
+        case CLRegionStateInside:
+            [self locationManager:manager didEnterRegion:region];
+            break;
+            
+        default:
+            break;
+    }
+}
+
+- (void)locationManager:(CLLocationManager *)manager didEnterRegion:(CLRegion *)region {
+    NSLog(@"Entered region");
+    [manager startRangingBeaconsInRegion:_beaconRegion];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didRangeBeacons:(NSArray *)beacons inRegion:(CLBeaconRegion *)region {
+    NSLog(@"Found some beacons");
+    for (CLBeacon *beacon in beacons) {
+        if (beacon.proximity == CLProximityImmediate) {
+            _closestBeacon = beacon;
+            NSLog(@"%@", _closestBeacon);
+        }
+    }
+    if (_closestBeacon == nil) {
+        NSLog(@"No beacons are within immediate proximity");
+    }
+    
+}
+
+
 
 @end
